a691b92a97b621312a308294e280bd1eaa5b6cbb
liamm91 <liamm91@kprschools.ca>
2020-04-14 23:28:44 -0400

    moved shape3d to src

---
+++ src/shape3d.cs
@@ -0,0 +1,64 @@
+using System;
+using color;
+using vector3;
+
+namespace shape3d
+{
+    public class Shape3d
+    {
+        // variables
+        private String _shape;
+        private Color _color;
+        private Vector3 _position;
+
+        // constructors
+        public Shape3d(String shape, Color color, Vector3 pos)
+        {
+            this._shape = shape;
+            this._color = color;
+            this._position = pos;
+        }
+
+        // empty constructor
+        public Shape3d() { }
+
+        // remove function, to delete set var to 'null'
+
+        // getters/setters
+        public String shape
+        {
+            get
+            {
+                return _shape;
+            }
+            set
+            {
+                _shape = value;
+            }
+        }
+
+        public Color color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+            }
+        }
+
+        public Vector3 position
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                _position = value;
+            }
+        }
+    }
+}
